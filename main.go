6057f93f84e7130fa478f7a4c5e0eff99a872ef5
Osvaldo Camacho <luis.cplascencia@alumnos.udg.mx>
2020-10-09 23:22:08 -0500

    Prueba 01

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var auxS int
+	var s []int
+	var sum int = 0
+	fmt.Scan(&n)
+	for i := 1; i <= n; i++ {
+		fmt.Scan(&auxS)
+		s = append(s, auxS)
+
+	}
+
+	for _, v := range s {
+		sum = sum + v
+
+	}
+	fmt.Println(sum)
 }
